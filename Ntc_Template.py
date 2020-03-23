c10e60cfec573207f0658340fd12e8eb1b17e084
Kerem Can Önemli <keremcanonemli@gmail.com>
2020-03-23 22:31:49 +0300

    NTC Show Vlan parse edilerek json formatına dönüştürüldü

--- Ntc_Template.py
+++ Ntc_Template.py
@@ -1,11 +1,18 @@
 from ntc_templates.parse import parse_output
-vlan_output = (
-        "VLAN Name                             Status    Ports\n"
-        "---- -------------------------------- --------- -------------------------------\n"
-        "1    default                          active    Gi0/1\n"
-        "10   Management                       active    \n"
-        "50   VLan50                           active    Fa0/1, Fa0/2, Fa0/3, Fa0/4, Fa0/5,\n"
-        "                                                Fa0/6, Fa0/7, Fa0/8\n"
-    )
+from netmiko import ConnectHandler
+import json
+SW1 = {
+    'device_type': 'cisco_ios',
+    'host':   '10.0.10.20',
+    'username': 'admin',
+    'password': 'Btegitim2020',
+    'port' : 22,
+    'secret': 'Btegitim2020'
+}
+connect=ConnectHandler(**SW1)
+vlan_output=connect.send_command("show vlan")
+
 vlan_parsed = parse_output(platform="cisco_ios", command="show vlan", data=vlan_output)
-print(vlan_parsed)
+print(json.dumps(vlan_parsed,indent=4))
+
+#print(vlan_parsed)
